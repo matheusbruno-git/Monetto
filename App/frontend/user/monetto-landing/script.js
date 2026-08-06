ba3f5d7aac44c10361664701acb45d95105034fa
Mizael Vinícios <miza.felis@gmail.com>
2026-08-06 11:11:22 -0300

    CheeseBread 1.2

--- App/frontend/user/monetto-landing/script.js
+++ App/frontend/user/monetto-landing/script.js
@@ -1,17 +1,16 @@
 document.getElementById("btn").addEventListener("click", async () => {
+  const dados = {
+    id_escola: "ID",
+    nome: document.getElementById("nome").value,
+    data_nascimento: document.getElementById("data").value,
+    cpf: document.getElementById("cpf").value,
+    responsavel: document.getElementById("resp").value,
+    telefone_responsavel: document.getElementById("tel").value,
+    email_responsavel: document.getElementById("email").value,
+    serie: document.getElementById("serie").value,
+  };
 
-    const dados = {
-        id_escola: "ID",
-        nome: document.getElementById("nome").value,
-        data_nascimento: document.getElementById("data").value,
-        cpf: document.getElementById("cpf").value,
-        responsavel: document.getElementById("resp").value,
-        telefone_responsavel: document.getElementById("tel").value,
-        email_responsavel: document.getElementById("email").value,
-        serie: document.getElementById("serie").value
-    };
+  const result = await window.api.registerAluno(dados);
 
-    const result = await window.api.registerAluno(dados);
-
-    document.getElementById("msg").innerText = result.message;
+  document.getElementById("msg").innerText = result.message;
 });
