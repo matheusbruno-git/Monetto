280c945637fb93abea1bebab4261626d42e3faea
Mizael Vinícios <miza.felis@gmail.com>
2026-08-13 11:07:01 -0300

    CheeseBread 1.74

--- App/frontend/student/login-aluno/script.js
+++ App/frontend/student/login-aluno/script.js
@@ -1,14 +1,14 @@
 function handleCredentialResponse(response) {
-    // JWT token Google returns
-    const token = response.credential;
+  // JWT token Google returns
+  const token = response.credential;
 
-    // Decode payload
-    const payload = JSON.parse(atob(token.split('.')[1]));
+  // Decode payload
+  const payload = JSON.parse(atob(token.split(".")[1]));
 
-    console.log("ID:", payload.sub);
-    console.log("Name:", payload.name);
-    console.log("Email:", payload.email);
-    console.log("Picture:", payload.picture);
+  console.log("ID:", payload.sub);
+  console.log("Name:", payload.name);
+  console.log("Email:", payload.email);
+  console.log("Picture:", payload.picture);
 
-    window.location.href = "../dashboard-aluno/dashboard-aluno.html";
+  window.location.href = "../dashboard-aluno/dashboard-aluno.html";
 }
